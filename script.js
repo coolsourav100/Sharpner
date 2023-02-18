94663ceea9f0d94cfd9f7204a8f0848ce0885aef
coolsourav100 <sarkar.sourav26jan@gmail.com>
2023-02-18 12:21:04 +0530

    sourav

---
+++ script.js
@@ -0,0 +1,6 @@
+let headTitle = document.getElementById('header-title')
+let head = document.getElementById('main-header')
+let item = document.getElementById('add-item')
+head.style.borderBottom = '3px solid black'
+item.style.color ='green'
+item.style.fontWeight ='bold'
