44e44c1dd9bd74aaca98400903a59812beb713f3
coolsourav100 <sarkar.sourav26jan@gmail.com>
2023-02-19 19:25:49 +0530

    Third

--- script.js
+++ script.js
@@ -4,8 +4,16 @@ let item = document.getElementById('add-item')
 head.style.borderBottom = '3px solid black'
 // item.style.color ='green'
 // item.style.fontWeight ='bold'
-let items = document.getElementsByClassName('list-group-item');
-items[2].style.backgroundColor = 'green'
+// let items = document.getElementsByClassName('list-group-item');
+// items[2].style.backgroundColor = 'green'
+// for(let i =0 ; i<items.length;i++){
+//   items[i].style.fontWeight = 'bold'
+// }
+let item1 = document.getElementsByClassName('list-group-item')
+for(let i=0;i<item1.length;i++){
+  item1[i].style.fontWeight ='bold'
+}
+let items = document.getElementsByTagName('li')
 for(let i =0 ; i<items.length;i++){
-  items[i].style.fontWeight = 'bold'
+  items[i].style.color = 'blue'
 }
