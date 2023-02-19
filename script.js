01c9cc4c7790266af190c18b30c98405e63dcbaf
coolsourav100 <sarkar.sourav26jan@gmail.com>
2023-02-19 19:14:34 +0530

    second

--- script.js
+++ script.js
@@ -2,5 +2,10 @@ let headTitle = document.getElementById('header-title')
 let head = document.getElementById('main-header')
 let item = document.getElementById('add-item')
 head.style.borderBottom = '3px solid black'
-item.style.color ='green'
-item.style.fontWeight ='bold'
+// item.style.color ='green'
+// item.style.fontWeight ='bold'
+let items = document.getElementsByClassName('list-group-item');
+items[2].style.backgroundColor = 'green'
+for(let i =0 ; i<items.length;i++){
+  items[i].style.fontWeight = 'bold'
+}
