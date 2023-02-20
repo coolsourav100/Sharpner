a715ba609655ab304b6228d344b8a35e533ba78c
Sourav <sourav.sarkar@hih7.com>
2023-02-20 15:03:49 +0530

    Ninth

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 let form = document.getElementById('my-form')
 form.addEventListener('submit',addUser=(e)=>{
     e.preventDefault();
-    console.log('click')
-    let name = document.getElementById('name').value 
-    let email = document.getElementById('email').value
-    window.localStorage.setItem('userDetails',`name: ${name} email: ${email}`)
+    // console.log('click')
+    let name1 = document.getElementById('name').value 
+    let email1 = document.getElementById('email').value
+    let userDetails = {
+        'name' : name1,
+        'email' : email1
+    }
+    localStorage.setItem('userDetails',JSON.stringify(userDetails))
+    console.log(email1)
 })
