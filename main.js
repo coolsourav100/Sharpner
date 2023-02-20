320f698ed0c8de9459f4204aeef5c34e1035d948
Sourav <sourav.sarkar@hih7.com>
2023-02-20 17:14:31 +0530

    Eleventh

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let form = document.getElementById('addform')
+let list = document.getElementById('list')
 form.addEventListener('submit',add=(e)=>{
     e.preventDefault()
  let name = document.getElementById('name').value 
@@ -11,5 +12,26 @@ form.addEventListener('submit',add=(e)=>{
     'phone' : phone
  }
 let userDetails = JSON.stringify(user)
-window.localStorage.setItem(email,userDetails)
+window.localStorage.setItem(phone,userDetails)
+// console.log(list)
+let deletebtn = document.createElement('button')
+deletebtn.className = 'btn btn-danger btn-sm float-right delete'
+deletebtn.appendChild(document.createTextNode('Delete'))
+let listitem = document.createElement('li')
+listitem.append(document.createTextNode(`Name : ${name} Email:${email} Phone:${phone}`))
+listitem.appendChild(deletebtn)
+list.appendChild(listitem)
 })
+
+ list.addEventListener('click',remove=(e)=>{
+    e.preventDefault()
+    if(e.target.classList.contains('delete')){
+        let li = e.target.parentElement
+        let ph = li.innerHTML.toString().split('Phone')[1].slice(1,11)
+        list.removeChild(li)
+        window.localStorage.removeItem(ph)
+        console.log(ph)
+        
+    }
+    // console.log(e.target.)
+ })
